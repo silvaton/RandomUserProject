17b35c67bd23d929e3d3bf296eccd0fccdab272e
silvaton <tdasilva@ext.laligatech.com>
2023-08-05 11:54:26 +0200

    feat(RUP-001): (Initial Setup)

---
+++ RandomUserProject/RandomUserProject/RandomUserProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserProjectApp.swift
+//  RandomUserProject
+//
+//  Created by Ton Silva on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
